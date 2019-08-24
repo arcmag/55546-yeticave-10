37bdc4d33ca6197a875ee6a6b92c6a35faad9938
Николай <arcmag-work@mail.ru>
2019-08-25 00:28:18 +0700

    Выполняет шаги из задания - 11. Работа со строкой запроса

--- data.php
+++ data.php
@@ -9,7 +9,7 @@ function get_categories($connect) {
 
 function get_announcement_list($connect) {
     $result = mysqli_query($connect, "
-        SELECT l.name as name, l.start_price, l.img, c.name as category, l.date_end
+        SELECT l.id, l.name as name, l.start_price, l.img, c.name as category, l.date_end
         FROM `lot` `l`
         LEFT JOIN `category` `c` ON c.id = l.category_id
         WHERE l.date_end > NOW()
@@ -17,4 +17,23 @@ function get_announcement_list($connect) {
         or die('Ошибка при обращении к базе данных: ' . mysqli_error());
 
     return mysqli_fetch_all($result, MYSQLI_ASSOC);
+}
+
+function get_lot_by_id($connect, $lot_id) {
+    $stmt = mysqli_prepare($connect, "
+        SELECT l.*, c.name as category
+        FROM `lot` `l`
+        LEFT JOIN `category` `c` ON c.id = l.category_id
+        WHERE l.id = ?");
+    mysqli_stmt_bind_param($stmt, 'i', $lot_id);
+    mysqli_stmt_execute($stmt);
+
+    return mysqli_fetch_assoc(mysqli_stmt_get_result($stmt)) ?? -1;
+}
+
+function get_wagers_by_lot_id($connect, $lot_id) {
+
+    return mysqli_fetch_all(mysqli_query(
+        $connect, "SELECT w.*, u.name as author FROM `wager` `w` LEFT JOIN `user` `u` ON u.id = w.author_id WHERE w.lot_id = $lot_id"),
+        MYSQLI_ASSOC);
 }
--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 
+require_once 'config/global.php';
 require_once 'config/db.php';
 
 require_once './helpers.php';
@@ -12,15 +13,45 @@ $connect = connect_db(DB_CONFIG);
 
 $categories = get_categories($connect);
 
-$main_page = include_template('main.php', [
+$page = $_GET['page'];
+
+$page_title = 'Главная страница';
+$page_template = include_template('main.php', [
     'categories' => $categories,
     'announcement_list' => get_announcement_list($connect)
 ]);
 
+if(DEBUG) {
+    debug($categories);
+}
+
+if(isset($page)) {
+    if($page === 'lot') {
+        $data_lot = get_lot_by_id($connect, $_GET['lot_id']);
+
+        if(DEBUG) {
+            debug($data_lot);
+        }
+
+        if($data_lot === -1) {
+            http_response_code(404);
+            $page_title = 'Ошибка 404: страница не найдена';
+            $page_template = include_template('404.php');
+        } else {
+            $page_title = $data_lot['name'];
+
+            $page_template = include_template('lot.php', [
+                'lot' => $data_lot,
+                'wagers' => get_wagers_by_lot_id($connect, $data_lot['id'])
+            ]);
+        }
+    }
+}
+
 print(include_template('layout.php', [
-    'title' => 'Главная страница',
+    'title' => $page_title,
     'is_auth' => rand(0, 1),
     'user_name' => $user_name,
     'categories' => $categories,
-    'content' => $main_page
+    'content' => $page_template
 ]));
